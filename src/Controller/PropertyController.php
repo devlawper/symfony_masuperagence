f17e66c7630ace973de5382d972296bb376234cf
Laurent Pichon <tech@lescachotieres.com>
2020-02-04 06:59:05 +0100

    feat : pagination & filtres

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,15 +19,31 @@ class PropertyController extends AbstractController
     /**
      * @Route("/biens", name="property_index")
      * @param PropertyRepository $propertyRepository
-     * @param EntityManagerInterface $manager
+     * @param PaginatorInterface $paginator
+     * @param Request $request
      * @return Response
      */
-    public function index(PropertyRepository $propertyRepository, EntityManagerInterface $manager)
+    public function index(PropertyRepository $propertyRepository, PaginatorInterface $paginator, Request $request)
     {
-        $property = $propertyRepository->findAllVisible();
+        $search = new PropertySearch();
+
+        $form = $this->createForm(PropertySearchType::class, $search);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump('ouii');
+        }
+
+        $property = $paginator->paginate(
+            $propertyRepository->findAllVisible($search),
+            $request->query->getInt('page', 1),
+            12
+            );
 
         return $this->render('property/index.html.twig', [
-            'current_menu' => 'properties'
+            'current_menu' => 'properties',
+            'properties' => $property,
+            'form' => $form->createView()
         ]);
     }
 
